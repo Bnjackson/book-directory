04b7361769e008cb41b20dfad4d3cc54cb82e3e0
Benjamin <bnjackson951@gmail.com>
2023-09-05 18:42:35 +0100

    Add functionality to get book index from user, check that input is a number and a book matches index

--- src/utils/input.js
+++ src/utils/input.js
@@ -1,11 +1,12 @@
 const readlineSync = require('readline-sync');
+const getModule = require('../commands/get.js');
 
 function getUserInput(question) {
     const userInput = readlineSync.question(`${question}`).toLowerCase();
     return userInput;
 }
 
-function getUserCommand () {
+function getUserCommand() {
     const userCommand = getUserInput('Enter a command? ');
     const allowedCommands = ['get', 'post', 'put', 'delete'];
     if (allowedCommands.includes(userCommand)) {
@@ -16,6 +17,21 @@ function getUserCommand () {
     }
 }
 
+async function getBookIndex(message) {
+    await getModule.outputBooks();
+    const bookData = await getModule.getBooks();
+    const inputtedIndex = Number(readlineSync.question(message));
+    if (isNaN(inputtedIndex)) {
+        console.log('ERROR: Input must be a number');
+    } else if (!bookData.books[inputtedIndex]) {
+        console.log('Error: Input must match a books index')
+    } else {
+        return inputtedIndex;
+    }
+    return getBookIndex(message)
+}
+
 module.exports = {
-    getUserCommand
+    getUserCommand,
+    getBookIndex
 }
