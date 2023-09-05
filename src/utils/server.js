b3a2ab2f4081c457a10bd666e54a45b48e59fbde
Benjamin <bnjackson951@gmail.com>
2023-09-05 18:40:11 +0100

    Add delete endpoint and get it up and running

--- src/utils/server.js
+++ src/utils/server.js
@@ -6,14 +6,25 @@ const port = process.env.port || 3000;
 
 app.get('/api/books', async (req, res) => {
     try {
-        const data = await databaseModule.readDatabase()
+        const data = await databaseModule.readDatabase();
         res.send(data);
     } catch (err) {
         console.error(`Error reading database:`, err.message);
     }
 });
 
+app.delete('/api/books/:index', async (req, res) => {
+    const bookToDelete = req.params.index;
 
+    try {
+        const data = await databaseModule.readDatabase(); 
+        data.books.splice(bookToDelete, 1);
+        await databaseModule.writeDatabase(data);
+        res.status(200).json({ message: 'Book deleted successfully' });
+    } catch (error) {
+        console.error(`Error deleting book ${error.message}`);
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
