5ee2216585404134e784daf0fe100c428e28139b
Benjamin <bnjackson951@gmail.com>
2023-09-06 14:48:30 +0100

    Add put and post endpoints, add express middleware

--- src/utils/server.js
+++ src/utils/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const databaseModule = require('../../db/database');
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.port || 3000;
 
 app.get('/api/books', async (req, res) => {
@@ -13,9 +15,34 @@ app.get('/api/books', async (req, res) => {
     }
 });
 
+app.put('/api/books/:index', async (req, res) => {
+    const index = req.params.index;
+    const updatedBook = req.body;
+    try {
+        const data = await databaseModule.readDatabase();
+        data.books[index] = updatedBook;
+        await databaseModule.writeDatabase(data);
+        res.status(200).json({ message: 'Book updated successfully' });
+    } catch (error) {
+        console.error(`Error updating book ${error}`);
+    }
+});
+
+app.post('/api/books', async (req, res) => {
+    const newBook = req.body;
+    try {
+        const data = await databaseModule.readDatabase();
+        data.books.push(newBook);
+        //Acess the newBook object from the request body
+        await databaseModule.writeDatabase(data);
+        res.status(200).json({ message: 'New book added successfully' });
+    } catch (error) {
+        console.error(`Error adding a new book ${error.message}`);
+    }
+});
+
 app.delete('/api/books/:index', async (req, res) => {
     const bookToDelete = req.params.index;
-
     try {
         const data = await databaseModule.readDatabase(); 
         data.books.splice(bookToDelete, 1);
@@ -24,7 +51,7 @@ app.delete('/api/books/:index', async (req, res) => {
     } catch (error) {
         console.error(`Error deleting book ${error.message}`);
     }
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
