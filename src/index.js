da58c402f1d608e8fac4ecff6807a8a66e16c4af
Benjamin <bnjackson951@gmail.com>
2023-09-07 14:56:30 +0100

    Import commands function, add conditional to call commands function, add anotherCommand function to ask user if wants to enter another command and calls main again

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const inputModules = require('./utils/input.js');
+const getModule = require('./commands/get.js');
+const postModule = require('./commands/post.js');
+const putModule = require('./commands/put.js');
+const deleteModule = require('./commands/delete.js');
 
 console.log(`
 This program stores a directory of books in a database, and uses a REST API to connect to and update the database from the command line. 
@@ -13,7 +17,28 @@ delete - deletes a book in the database
 
 async function main() {
     const userCommand = inputModules.getUserCommand();
-    console.log(userCommand);
+    if (userCommand === 'get') {
+        await getModule.outputBooks();
+    } else if (userCommand === 'post') {
+        await postModule.postBook();
+    } else if (userCommand === 'put') {
+        await putModule.putBook();
+    } else if (userCommand === 'delete') {
+        await deleteModule.deleteBook();
+    }
+    anotherCommand();
+}
+
+function anotherCommand() {
+    const userAnswer = inputModules.getUserInput('Would you like to enter another command? y/n ');
+    if (userAnswer.toLowerCase() === 'y') {
+        main();
+    } else if (userAnswer.toLowerCase() === 'n') {
+        console.log('Thanks for using this program');
+    } else {
+        console.log(`ERROR: Input must be either y/n`);
+        return anotherCommand();
+    }
 }
 
 main();
