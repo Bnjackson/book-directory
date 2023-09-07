b9b374c6f95ba22f9b579dd382daaa76b9633180
Benjamin <bnjackson951@gmail.com>
2023-09-07 15:55:17 +0100

    Fix error messages change err to error

--- db/database.js
+++ db/database.js
@@ -8,7 +8,7 @@ async function readDatabase() {
         const jsonData = fs.readFileSync(databaseFilePath, 'utf-8');
         return JSON.parse(jsonData);
     } catch (error) {
-        console.error(`Error reading database: ${err}`);
+        console.error(`Error reading database: ${error}`);
         throw error;
     }
 } 
@@ -17,7 +17,7 @@ async function writeDatabase(data) {
     try {
         fs.writeFileSync(databaseFilePath, JSON.stringify(data, null, 2), 'utf-8');
     } catch (error) {
-        console.error(`Error writing to database: ${err}`)
+        console.error(`Error writing to database: ${error}`)
         throw error;
     }
 }
